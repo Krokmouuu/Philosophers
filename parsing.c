403ab1978f03a885f6b57b078d44af993f6f1504
Benoit Leroy <bleroy@c2r8p2.42nice.fr>
2022-05-31 15:57:56 +0200

    PUSHED 100% DAMN

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: bleroy <bleroy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 15:07:39 by bleroy            #+#    #+#             */
-/*   Updated: 2022/05/27 15:08:05 by bleroy           ###   ########.fr       */
+/*   Updated: 2022/05/31 15:56:50 by bleroy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,28 +24,28 @@ void	parseattrib(t_all *philo, int argc, char **argv)
 
 int	parsing2(int argc, t_all *philo)
 {
-	if (philo->parse.nb_philo < 0)
+	if (philo->parse.nb_philo <= 0)
 	{
 		printf("Missing philosophers\n");
 		return (0);
 	}
-	if (philo->parse.time_die < 0)
+	if (philo->parse.time_die <= 0)
 	{
 		printf("Death timer invalid\n");
 		return (0);
 	}
-	if (philo->parse.time_eat < 0)
+	if (philo->parse.time_eat <= 0)
 	{
 		printf("Eat timer invalid\n");
 		return (0);
 	}
-	if (philo->parse.time_sleep < 0)
+	if (philo->parse.time_sleep <= 0)
 	{
 		printf("Sleep timer invalid\n");
 		return (0);
 	}
 	if (argc == 6)
-		if (philo->parse.nb_eat < 0)
+		if (philo->parse.nb_eat <= 0)
 			return (0);
 	return (1);
 }
